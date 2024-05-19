ea71c46ad66f17a6741c6e18c6e7d7011ed724db
danivrb <152440187+danivrb@users.noreply.github.com>
2024-05-19 22:25:47 +0300

    geocode.js

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/search/geocode/v6/forward?q='+ encodeURIComponent(address) + '&access_token=pk.eyJ1IjoiZGFuaXZyYiIsImEiOiJjbHYzbm11OWowcmUzMmtwMHBhaTc1Y3A4In0.GCrDxL7wFC17aH4eyZAx0w&limit=1'
+    const url = 'https://api.mapbox.com/search/geocode/v6/forward?q='+ encodeURIComponent(address) + '&access_token=pk.YOUR-ACCES-TOKEN-FROM-MAPBOX&limit=1'
     request({url: url, json: true}, (error, { body }) => {
         if (error){
             callback('Unable to connect to location services!')
